aa2eb012be7f2babd7456916f9b27d51a87319d6
darshanlal2k <darshanlal2k@gmail.com>
2023-12-03 12:09:12 +0530

    use import for image instead of using src

--- udemy_projects/01-starting-project/src/App.jsx
+++ udemy_projects/01-starting-project/src/App.jsx
@@ -1,3 +1,5 @@
+import reactImage from './assets/react-core-concepts.png';
+
 const reactDescriptions = ['Fundamentals', 'Crucial', 'Core'];
 
 function getRandomInt(max) {
@@ -9,7 +11,7 @@ function Header() {
   const description = reactDescriptions[getRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
