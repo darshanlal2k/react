e8a3c349d152b7c7b82adf59e2bf57ac14fde13c
darshanlal2k <darshanlal2k@gmail.com>
2023-12-03 21:43:09 +0530

    used props from data.js file

--- udemy_projects/01-starting-project/src/App.jsx
+++ udemy_projects/01-starting-project/src/App.jsx
@@ -1,5 +1,6 @@
 import reactImage from './assets/react-core-concepts.png';
 import componentimg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
 const reactDescriptions = ['Fundamentals', 'Crucial', 'Core'];
 
 function getRandomInt(max) {
@@ -40,13 +41,25 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept
-              title='darshan'
-              description='fjhakjdfh'
-              img={componentimg}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              img={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept title='props' />
-            <CoreConcept />
-            <CoreConcept />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              img={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              img={CORE_CONCEPTS[2].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              img={CORE_CONCEPTS[3].image}
+            />
           </ul>
         </section>
 
