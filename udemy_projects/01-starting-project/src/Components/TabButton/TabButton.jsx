b03f49c3152687196f46ff21979626e0518c7272
darshanlal2k <darshanlal2k@gmail.com>
2023-12-11 18:04:24 +0530

    react events checking

--- udemy_projects/01-starting-project/src/Components/TabButton/TabButton.jsx
+++ udemy_projects/01-starting-project/src/Components/TabButton/TabButton.jsx
@@ -1,7 +1,15 @@
-export default function TabButton({children}) {
+export default function TabButton({ children }) {
+    // instead of using this click event (vanilla javascript ) . 
+    // react gives a special attribute called props (onclick)
+    // document.querySelector('button').addEventListener('click', () => {
+    // })
+
+    function handleClick() {
+        console.log("Hello React Events");
+    }
     return (
         <li>
-            <button>{children}</button>
+            <button onClick={handleClick} >{children}</button>
         </li>
     );
 }
