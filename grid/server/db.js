bb4e4f976bcbad7d5df2d1dfc77b352e5444f846
darshanlal2k <darshanlal2k@gmail.com>
2024-01-03 13:07:02 +0530

    comments used in db.js file

--- grid/server/db.js
+++ grid/server/db.js
@@ -1,4 +1,9 @@
+// db.js-> This file is about connecting Server(NodeJs) and Database (PostgreSQL) - Darshan
+
+// install and import postgresql (pg) - Darshan
 const { Client } = require('pg')
+
+// connecting query with database details like host,user,password,database name,port number  - Darshan
 const connection = new Client({
     host: 'localhost',
     user: 'postgres',
@@ -6,8 +11,11 @@ const connection = new Client({
     database: 'hospital',
     port: 5432,
 });
+
+// checking database is connected or not - Darshan
 connection.connect(function (err) {
     err ? console.log("Database not connected") : console.log("Database connected");
 });
 
+// as above database connection just we exporting because to access data, in other files  - Darshan
 module.exports = connection;
